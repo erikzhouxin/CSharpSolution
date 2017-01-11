2332b44270027aed75596f39f171e550b9e77bc8
erikzhouxin <ezx@erikzhouxin.com>
2017-01-11 22:49:53 +0800

    修改项目,调整项目使用

---
+++ Framework/MyBatis/WebSI/App_Start/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EZOper.TechTester.MyBatisWebSI.Startup))]
+namespace EZOper.TechTester.MyBatisWebSI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Framework/MyBatis/WebSI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EZOper.TechTester.MyBatisWebSI.MvcApplication" Language="C#" %>
