7642144826a894cfad7a9c64a97d0e5e89bd933b
erikzhouxin <ezx@erikzhouxin.com>
2017-03-14 01:01:13 +0800

    添加Visual Studio 2017的Android的技术探究集

---
+++ Android/VS/CnaAppSI/Activities/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using EZOper.TechTester.Adapters;
+
+namespace EZOper.TechTester.Activities
+{
+    [Activity(Label = "技术探究集", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        internal static readonly ReadOnlyDictionary<int, Type> AreaDic = new ReadOnlyDictionary<int, Type>(new Dictionary<int, Type>()
+        {
+            {0, typeof(TTTMainListViewActivity)}
+        });
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // 找到页面元素
+            var _functionListView = FindViewById<ListView>(Resource.Id.LvwAreaNameList);
+            if (_functionListView != null)
+            {
+                _functionListView.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) =>
+                {
+                    Type areaName = MainActivity.AreaDic[e.Position];
+                    var taskDetails = new Intent(this, areaName);
+                    StartActivity(taskDetails);
+                };
+            }
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            var areaListView = FindViewById<ListView>(Resource.Id.LvwAreaNameList);
+            //Hook up our adapter to our ListView
+            areaListView.Adapter = new TypeDictionaryAdapter(this, AreaDic);
+        }
+    }
+}
+
---
+++ Android/VS/CnaAppSI/Activities/TTT/TTTAnalogClockActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using EZOper.TechTester.ViewModels;
+using EZOper.TechTester.Models;
+
+namespace EZOper.TechTester.Activities
+{
+	[Activity (Label = "模拟钟表")]
+	public class TTTAnalogClockActivity : Activity
+	{
+		AnalogClockViewModel clockView;
+        AnalogClockModel clockModel;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set content view to FrameLayout with ClockView.
+			FrameLayout frameLayout = new FrameLayout (this);
+			clockView = new AnalogClockViewModel (this);
+			frameLayout.AddView (clockView);
+			SetContentView (frameLayout);
+
+			// Create ClockModel to keep clock updated.
+			clockModel = new AnalogClockModel
+            {
+				IsSweepSecondHand = true
+			};
+
+			// Initialize clock.
+			SetClockHandAngles ();
+
+			clockModel.PropertyChanged += (object sender, PropertyChangedEventArgs e) => 
+			{
+				// Update clock.
+				SetClockHandAngles();
+			};
+		}
+
+		void SetClockHandAngles()
+		{
+			clockView.SetClockHandAngles(clockModel.HourAngle, 
+										 clockModel.MinuteAngle, 
+										 clockModel.SecondAngle);
+		}
+	}
+}
+
+
