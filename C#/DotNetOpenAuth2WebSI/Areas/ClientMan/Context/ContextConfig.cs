87e62e5c99331e1849d4c224dfd994ca969353cd
Bigsail44 <Bigsail44@liangzuo.com>
2017-04-12 15:49:14 +0800

    C#:     添加DotNetOpenAuth2WebSI项目,项目功能与OAuth2WebSI相似,属于集成项目未进行调试运行     移除Framework目录,其中MyBatis.Net.Demo为弃用项目,以后学习时再进行集成

---
+++ C#/DotNetOpenAuth2WebSI/Areas/ClientMan/Context/ContextConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace EZOper.TechTester.DotNetOpenAuth2WebSI.Areas.ClientMan
+{
+    public class ContextConfig
+    {
+        public static string OpenApiAddress = ConfigurationManager.AppSettings["OpenApiAddress"];
+
+    }
+}
---
+++ C#/DotNetOpenAuth2WebSI/Areas/Clients/Context/ContextConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace EZOper.TechTester.DotNetOpenAuth2WebSI.Areas.Clients
+{
+    public class ContextConfig
+    {
+        public static string TokenEndpoint = ConfigurationManager.AppSettings["TokenEndpoint"];
+        public static string AuthorizationEndpoint = ConfigurationManager.AppSettings["AuthorizationEndpoint"];
+        /// <summary>
+        /// OpenApiAddress = OpenApiAddress.EndsWith("/") ? OpenApiAddress : (OpenApiAddress + "/");
+        /// </summary>
+        public static string OpenApiAddress = ConfigurationManager.AppSettings["OpenApiAddress"];
+    }
+}
