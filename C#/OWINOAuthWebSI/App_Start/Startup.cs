193c378670cc67d58f4e36f783b0825729e57f6f
Bigsail44 <Bigsail44@liangzuo.com>
2017-04-12 18:24:08 +0800

    C#:     添加OWINOAuthWebSI项目,依据[https://github.com/yuezhongxin/OAuth2.Demo]中Demo集成

---
+++ C#/OWINOAuthWebSI/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartupAttribute(typeof(EZOper.TechTester.OWINOAuthWebSI.Startup))]
+namespace EZOper.TechTester.OWINOAuthWebSI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            var configuration = new HttpConfiguration();
+            WebApiConfig.Register(configuration);
+            app.UseWebApi(configuration);
+        }
+    }
+}
---
+++ C#/OWINOAuthWebSI/Areas/Home/Controllers/ReadmeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EZOper.TechTester.OWINOAuthWebSI.Areas.Home.Controllers
+{
+    /// <summary>
+    /// 项目引用:https://github.com/yuezhongxin/OAuth2.Demo
+    /// GET: Readme
+    /// </summary>
+    public class ReadmeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
