bc9bef0ce3504203687d1b16374e108ff6073b91
Bigsail45 <erikzhouxin@qq.com>
2017-09-08 19:37:39 +0800

    添加时间日期控件

---
+++ WebUI/Areas/Help/Controllers/DatePickerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EZOper.CSharpSolution.WebUI.Areas.Help.Controllers
+{
+    /// <summary>
+    /// 时间选择控制器
+    /// GET: Help/DatePicker
+    /// </summary>
+    public class DatePickerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BsDatePicker()
+        {
+            return View();
+        }
+
+        public ActionResult BsDateTimePicker()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebUI/Scripts/EZX/ezoper.datetime.js
@@ -0,0 +1,15 @@
+﻿//往往json传过来的时间都是"/Date(1405056837780)/"
+//转换需要的方法
+String.prototype.ToString = function (format) {
+	var dateTime = new Date(parseInt(this.substring(6, this.length - 2)));
+	format = format.replace("yyyy", dateTime.getFullYear());
+	format = format.replace("yy", dateTime.getFullYear().toString().substr(2));
+	format = format.replace("MM", dateTime.getMonth() + 1)
+	format = format.replace("dd", dateTime.getDate());
+	format = format.replace("hh", dateTime.getHours());
+	format = format.replace("mm", dateTime.getMinutes());
+	format = format.replace("ss", dateTime.getSeconds());
+	format = format.replace("ms", dateTime.getMilliseconds())
+	return format;
+};
+//调用"/Date(1405056837780)/".ToString("yyyy年MM月dd日hh时mm分ss秒");
